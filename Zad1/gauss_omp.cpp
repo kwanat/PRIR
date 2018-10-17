5a223831c0ac90919a4c2463c423ce53547a6481
Szczupii <dr.szczupak@gmail.com>
2018-10-17 13:00:25 +0200

    Update Zad1/gauss_omp.cpp

--- Zad1/gauss_omp.cpp
+++ Zad1/gauss_omp.cpp
@@ -26,7 +26,7 @@ int DivSumMask(int value, int SumMask) {
 int main(int argc, char **argv)
 {
 	int column, row, R, G, B, x, y;
-   	const int mask[5][5] = {
+   	const int mask[5][5] = {			
    		{1,1,2,1,1},
         {1,2,4,2,1},
         {2,4,8,4,2},
@@ -34,7 +34,7 @@ int main(int argc, char **argv)
   		{1,1,2,1,1}
 	}; 
 int SumMask = 52;
-double begin_time, end_time, total_time;
+	double begin_time, end_time, total_time;
 
 	if(argc!=4){
 		cout << "Niepoprawna liczba oargumentow" << argc;
