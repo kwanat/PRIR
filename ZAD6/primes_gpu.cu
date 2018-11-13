05d61e663bab2f2d1403d525b64d7b31b3c2d220
Dziapo <wanat.kamil@wp.pl>
2018-11-13 22:12:08 +0100

    zad 6 not working

--- ZAD6/primes_gpu.cu
+++ ZAD6/primes_gpu.cu
@@ -16,16 +16,16 @@ struct number{		//struktura wykorzystywana w wektorze danych - zawiera informacj
     bool prime;
 };
 
-__global__ void primeTesting (vector<number>* tab, uint sqr, uint d)         //Funkcja testująca pierwszoć liczb, przyjmuje jako argumenty zbior do tesowania, pierwiastek do ktorego testujemy oraz wielkosc zbioru testowanego
-{
+__global__ void primeTesting (number* tab, uint sqr, uint d)         //Funkcja testująca pierwszoć liczb, przyjmuje jako argumenty zbior do tesowania, pierwiastek do ktorego testujemy oraz wielkosc zbioru testowanego
+{    
 uint tid=blockIdx.x;                                                                          //Funkcja zwraca przetestowany zbior
 uint i,j;
 for (i=2;i<=sqr;i++) {			//petla zrownoleglana - kolejne liczby od 2 do pierwiastka kwadratowego z najwiekszego elementu zbioru wczytywanego
         for (j = tid; j <d; j+=blockDim.x) {		//petla wewnetrzna sprawdzajaca czy kolejne liczby wektora tab dziela sie przez aktualna wartosc zmiennej i
                 if((tab[j].value%i==0)&(tab[j].value!=i)) //jesli tak liczba uznawana jest za zlozona, dodatkowo sprawdzamy czy liczba nie jest rowna obecnemu dzielnikowi (zasada pierwszosci)
-                    tab[j].prime=false;
+                    tab[j].prime=false;    
         }
-    }
+    }                                      
 }
 
 int main(int argc, char** argv) {
@@ -67,26 +67,43 @@ int main(int argc, char** argv) {
     uint sqr=sqrt(maxval);			//pierwiastek z liczby maksymalnej
     uint d=tab.size();          //zmienna pomocnicza rozmiar wektora danych
 
-    vector<number>* tab2;
+    number* tab2;
     error=cudaMalloc( (void**)&tab2, d * sizeof(number) );
-    error = cudaMemcpy(tab2, tab, d * sizeof(number), cudaMemcpyHostToDevice);
+    error = cudaMemcpy(tab2, &tab[0], d * sizeof(number), cudaMemcpyHostToDevice);
     cudaEventRecord(start);
-
-    primeTesting <<< blockNumber, 1 >>> (tab2, sqr,d);
+    cout<<d<<endl<<sizeof(tab)<<endl;
+    //primeTesting <<< blockNumber, 1 >>> (tab2, sqr,d);
 
     cudaEventRecord(stop); //zatrzymanie licznika i zczytanie czasu obliczen
     error = cudaEventSynchronize(stop);
-    vector<number>* reult= new vector<number>();
-    result->reserve( d );
-    error = cudaMemcpy(result, tab2, d * sizeof(number), cudaMemcpyDeviceToHost);
+    vector<number>* result=new vector<number>();
+    //vector<number> * result;
+    //result= (vector<number> *) malloc (d*sizeof(number));
+    
+    //vector<number> result;
+    result->resize( d );
+    error = cudaMemcpy(result, &tab2, d * sizeof(number), cudaMemcpyDeviceToHost);
     cudaEventElapsedTime(&elapsedTime, start, stop);
     printf("Czas : %f ms\n", elapsedTime);
-    for (uint i=0;i<(* result).size();i++)				//wypisanie liczb z  wektora tab wraz z informacją czy są pierwsze
+    
+    cout<<result->size()<<endl;
+    cout<<sizeof(result)<<endl;
+    cout<<sizeof(number)<<endl;
+    cout<<endl<<endl<<(*result)[0].value<<endl;
+       
+    for (uint i=0;i<10;i++)				//wypisanie liczb z  wektora tab wraz z informacją czy są pierwsze
         if((* result)[i].prime==true)
             cout<<(* result)[i].value<<": prime"<<endl;
         else
             cout<<(* result)[i].value<<": composite"<<endl;
-	                  
+                 
+           /*          
+            for (uint i=0;i<10;i++)				//wypisanie liczb z  wektora tab wraz z informacją czy są pierwsze
+        if(tab[i].prime==true)
+            cout<<tab[i].value<<": prime"<<endl;
+        else
+            cout<<tab[i].value<<": composite"<<endl;
+	         */          
     return 0;                  
 
 }
